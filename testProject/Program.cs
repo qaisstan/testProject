f274b2769b4e220b982e5f4d76801ee4f5d2320e
qaisstan <94216834+qaisstan@users.noreply.github.com>
2022-09-07 17:10:20 +0200

    Initial commit

---
+++ testProject/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string du = "mäta in något";
+
+
+Console.WriteLine(du);
+string not = Console.ReadLine();
+
+Console.WriteLine($" {not } {du} ");
